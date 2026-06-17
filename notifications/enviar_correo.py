f4d0e74e050f2051060682bb1174798b539943b3
AdaraAtenea <sheylamendez12@gmail.com>
2026-06-16 23:53:19 -0600

    Sprint 9 empezamos a automatizar el envio de datos cuando entra una nueva vacante, por el momento solo manual

--- notifications/enviar_correo.py
+++ notifications/enviar_correo.py
@@ -3,63 +3,83 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-# CORREO EMISOR
-correo_emisor = "jobhunterai.alerts@gmail.com"
+def enviar_correo(
+    titulo,
+    empresa,
+    compatibilidad,
+    score,
+    url_vacante
+):
 
-# APP PASSWORD DE GOOGLE
-password = "jbqa mdpt fenz nzvd"
+    # CORREO EMISOR
+    correo_emisor = "jobhunterai.alerts@gmail.com"
+    # APP PASSWORD "jbqa mdpt fenz nzvd"
+    password = "jbqa mdpt fenz nzvd"
+    # DESTINO
+    correo_receptor = "ramiresmzhey12@gmail.com"
+    # CREAR MENSAJE
+    mensaje = MIMEMultipart()
 
-# CORREO DESTINO
-correo_receptor = "ramiresmzhey12@gmail.com"
+    mensaje["From"] = correo_emisor
+    mensaje["To"] = correo_receptor
+    mensaje["Subject"] = (
+        f"🚀 Nueva vacante: {titulo}"
+    )
 
-# CREAR MENSAJE
-mensaje = MIMEMultipart()
+    contenido = f"""
+Nueva vacante encontrada 🚀
 
-mensaje["From"] = correo_emisor
-mensaje["To"] = correo_receptor
-mensaje["Subject"] = "🚀 Prueba Job Hunter AI"
+Puesto:
+{titulo}
 
-contenido = """
-Hola.
+Empresa:
+{empresa}
 
-Este es el primer correo automático enviado
-desde Job Hunter AI usando Python SMTP.
+Compatibilidad:
+{compatibilidad}%
 
-Sprint 9 iniciado correctamente 🚀
+Score:
+{score}
+
+Aplicar aquí:
+{url_vacante}
 """
 
-mensaje.attach(
-    MIMEText(contenido, "plain")
-)
-
-try:
-
-    # CONECTAR CON GMAIL SMTP
-    servidor = smtplib.SMTP(
-        "smtp.gmail.com",
-        587
+    mensaje.attach(
+        MIMEText(contenido, "plain")
     )
 
-    servidor.starttls()
+    try:
 
-    # LOGIN
-    servidor.login(
-        correo_emisor,
-        password
-    )
+        servidor = smtplib.SMTP(
+            "smtp.gmail.com",
+            587
+        )
 
-    # ENVIAR
-    servidor.sendmail(
-        correo_emisor,
-        correo_receptor,
-        mensaje.as_string()
-    )
+        servidor.starttls()
 
-    print("\n✅ Correo enviado correctamente")
+        servidor.login(
+            correo_emisor,
+            password
+        )
 
-    servidor.quit()
+        servidor.sendmail(
+            correo_emisor,
+            correo_receptor,
+            mensaje.as_string()
+        )
 
-except Exception as e:
+        servidor.quit()
 
-    print("\n❌ Error al enviar correo:")
-    print(e)
+        print(
+            "✅ Correo enviado:",
+            titulo
+        )
+
+    except Exception as e:
+
+        print(
+            "❌ Error al enviar correo:"
+        )
+
+        print(e)
---
+++ test_correo.py
@@ -0,0 +1,9 @@
+from notifications.enviar_correo import enviar_correo
+
+enviar_correo(
+    "Junior PHP Developer",
+    "Empresa Demo",
+    78,
+    92,
+    "https://remoteok.com/demo"
+)
